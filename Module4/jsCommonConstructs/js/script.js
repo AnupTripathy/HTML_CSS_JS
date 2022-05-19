b6ed9e1f79f8a2b3577a6d3dadb235a8a1c6cfd2
Anup <anuptripathy04@gmail.com>
2022-05-19 21:44:27 +0530

    Commit Changes"

---
+++ Module4/jsCommonConstructs/js/script.js
@@ -0,0 +1,58 @@
+// string concatination
+// var string = "Hello";
+// string+=" World";
+// string+="!";
+// console.log(string);
+
+//math operations(+, - , *, /)
+// console.log((5+7)/3);
+// console.log(null/4);
+// console.log(undefined/8);
+// console.log(5/0);
+// console.log(6 * 1/2);
+// function half(x) {
+//     console.log(x * 1/2);
+// }
+
+// half();
+// half(0);
+// half("string");
+// half(22);
+// half(null);
+
+// Equality Check
+// var x = 10, y=10;
+// if (x==y) {
+//     console.log("x="+x+" is equal to y="+y)
+// }
+
+// x = "10";
+
+// if (x==y) {
+//     console.log("x is again equal to y");
+// }
+
+// '==' will not check the number and string strictly //but mostly we're using this operator
+
+// Strict Equality Check
+
+// if (x===y) {
+//     console.log("Strictly: x and y is equal");
+// }
+
+// else {
+//     console.log("Strictly : x is NOT-equal to y");
+// }
+
+
+
+// For Loop
+
+var sum = 0;
+
+for (var i=0; i<6; i++) {
+    console.log(i);
+    sum += 1;
+}
+
+
