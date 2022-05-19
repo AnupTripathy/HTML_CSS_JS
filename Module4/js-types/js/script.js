78ea0840e3c5c00b7b136f05e619d2fdc9559a95
Anup <anuptripathy04@gmail.com>
2022-05-19 12:44:05 +0530

    Commit Chnages

---
+++ Module4/js-types/js/script.js
@@ -0,0 +1,35 @@
+var x;
+var b = true;
+var n = null;
+var s = "Something"
+console.log(x);
+
+// x = 2;
+
+// n = 3;
+b = false;
+// s = "Nothing"
+
+
+if (x == undefined) {
+    console.log("x is undefined");
+}
+else {
+    console.log("All Defined");
+}
+
+if (b == true) {
+    console.log("B is True");
+}
+else {
+    console.log("B is False");
+}
+
+if (n == null) {
+    console.log("There is nothing in n");
+}
+else {
+    console.log("n is not null")
+}
+
+console.log(s)
