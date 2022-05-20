7d64a80cca13c2730d325d28fde01ecde3e3ab4c
Anup <anuptripathy04@gmail.com>
2022-05-20 14:40:24 +0530

    Commit Changes"

--- Module4/ObjectsNfunctions/Functions/script.js
+++ Module4/ObjectsNfunctions/Functions/script.js
@@ -11,6 +11,12 @@ makeMultiplier.version = "v1.0.0";
 
 var Times3 = makeMultiplier(3); //arg multiplier is now 3
 console.log(Times3(4)); //result arg x value is 4
-var double = makeMultiplier(2);
-console.log(double(2500));
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(2500));
 console.log(makeMultiplier.version);
+
+function DoOperation(x, operation) {
+    return operation(x);
+}
+
+console.log(DoOperation(4, doubleAll))
