8497fa72b0fd3f9843915ff09d0c5b350f49dc62
Anup <anuptripathy04@gmail.com>
2022-05-20 11:05:11 +0530

    Commit Changes

--- Module4/ObjectsNfunctions/Functions/script.js
+++ Module4/ObjectsNfunctions/Functions/script.js
@@ -1,6 +1,18 @@
-
 function multiply(x,y) {
     console.log(x*y);
 }
 
-multiply(6,9);
+multiply.version = "v1.0.0";
+// Function is a object
+
+function makeMultiplier(multiplier) {
+    var result = function (x) {
+        return multiplier * x;
+    };
+
+    return result
+}
+
+
+var Times3 = makeMultiplier(3); //arg multiplier is now 3
+console.log(Times3(4)); //result arg x value is 4
