7433d01c50156eb6bc9f3143384d49185fb70088
Anup <anuptripathy04@gmail.com>
2022-05-20 08:16:41 +0530

    Commit Changes

---
+++ Module4/JsUserMadeDefaultValue/script.js
@@ -0,0 +1,7 @@
+function OrderChicken_with(SideDish) {
+    SideDish = SideDish || "current popular dish!"
+    console.log("You'll get Chicken with "+SideDish+" within two-three hours..")
+}
+
+OrderChicken_with("Momo")
+OrderChicken_with()
